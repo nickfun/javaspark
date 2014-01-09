5f9bf8e7569727d8aaf00baeedc6126bca801119
Nick F <nick@nick.gs>
2014-01-08 22:19:48 -0800

    Add Maven Shade so I can build an Uber Jar

--- src/main/java/gs/nick/HelloWorld.java
+++ src/main/java/gs/nick/HelloWorld.java
@@ -41,27 +41,3 @@ public class HelloWorld {
 	}
 
 }
-
-class MyData {
-	private String name;
-	private String msg;
-	private int age;
-	public void setName(String n) {
-		name = n;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setMsg(String c) {
-		msg = c;
-	}
-	public String getMsg() {
-		return msg;
-	}
-	public void setAge(int a) {
-		age=a;
-	}
-	public int getAge() {
-		return age;
-	}
-}
---
+++ src/main/java/gs/nick/MyData.java
@@ -0,0 +1,31 @@
+package gs.nick;
+
+class MyData {
+	private String name;
+	private String msg;
+	private int age;
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setMsg(String c) {
+		msg = c;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setAge(int a) {
+		age = a;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
